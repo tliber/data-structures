4d8e1396b2a3e6e13414206313a3af7bae0e6fb2
Hack Reactor Students <accounts@hackreactor.com>
2014-12-14 19:33:04 -0800

    almost done just need to bind

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -16,6 +17,12 @@ HashTable.prototype.insert = function(k, v){
   }
 
   this._storage.get(i).push(tup);
+  this._count++;
+  if (this._count >= (this._limit * .75)){
+    var newLimit = (this._limit * .75);
+    // this._limit = this._limit * 2;
+    this.resize(this._limit);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -45,7 +52,41 @@ HashTable.prototype.remove = function(k){
     }
   }
 };
+HashTable.prototype.resize = function(limit){
 
+  // console.log(this._storage);
+  var pairCopies = [];
+  this._storage.each(function(value,index,storage){
+    if (value === undefined){return null};
+    for(var k in value){
+      pairCopies.push((value[k]));
+    }
+  })
+  this._storage = LimitedArray(limit);
+  // this._count = 0;
+  for (var pair in pairCopies){
+     var key = pair[0];
+     var val = pair[1];
+     this.insert(key, val);
+    }
+  // console.log(this._storage.get(0));
+
+  // _.each(this._storage,function(bucket){
+    // console.log(this._storage(bucket));
+    // for(var cnt = 0; tup < bucket.length;cnt++){
+      // console.log(bucket(cnt)[0]);
+    // }
+
+      // console.log(bucket);
+    // }, pairCopies);
+
+
+
+
+  // });
+
+
+}
 
 
 /*
